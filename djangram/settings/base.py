05c0fc9b21b5b4283ce86195621bf93c71740999
pamella <pammbsilva@gmail.com>
2019-08-01 11:48:17 -0300

    try

--- djangram/settings/base.py
+++ djangram/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -159,3 +160,37 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+
+# social_django
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.instagram.InstagramOAuth2',
+
+    # django
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+# ID do cliente
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '129312100772-cg9kup64i6vkhq99om9nqcqr48ogq4eo.apps.googleusercontent.com'
+# Chave secreta do cliente
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'Hi0lpz-1jtLb6rTYlVskMidC'
+
+
+# Email
+EMAIL_HOST_USER = 'testedjangojr@gmail.com'
+EMAIL_HOST_PASSWORD = 'senhaTEST'
+
+
+django_heroku.settings(locals())
--- djangram/settings/production.py
+++ djangram/settings/production.py
@@ -1,50 +1,21 @@
-from decouple import config
-import django_heroku  # Configure Django App for Heroku.
-
-from djangram.settings.base import *
-
-
-SECRET_KEY = config('SECRET_KEY')
+from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '.herokuapp.com',
-]
+ALLOWED_HOSTS = ['.herokuapp.com']
 
-# social_django
-AUTHENTICATION_BACKENDS = (
-    'social_core.backends.google.GoogleOAuth2',
-    'social_core.backends.instagram.InstagramOAuth2',
-
-    # django
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.social_auth.associate_by_email',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-)
-
-# ID do cliente
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '129312100772-cg9kup64i6vkhq99om9nqcqr48ogq4eo.apps.googleusercontent.com'
-# Chave secreta do cliente
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'Hi0lpz-1jtLb6rTYlVskMidC'
-
-# Instagram
-# https://www.instagram.com/developer/authentication/
-
-
-# Email
-EMAIL_HOST_USER = 'testedjangojr@gmail.com'
-EMAIL_HOST_PASSWORD = 'senhaTEST'
-
-
-django_heroku.settings(locals())
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
